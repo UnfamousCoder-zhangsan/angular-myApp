f9e4483e99f75e749c20f12d4aa08f93e6700747
lsircode <1546294949@qq.com>
2019-09-20 18:28:54 +0800

    列表展示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  title = 'Tour of Heroes'
+  name  = '英雄指南'
 }
